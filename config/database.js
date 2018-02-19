83e4a85ffbbf33c25fd3954ed77b44474df648e4
bachirbiaich <bachir.biaich@gmail.com>
2018-02-19 10:30:23 +0100

    ajout connexion BDD en ligne

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/foot',
+    database: 'mongodb+srv://pronoadmin:pronopassynov@pronofoot-raenv.mongodb.net/pronofoot',
     secret: 'secret'
 }
 
+//MONGODB COMPASS connection string : 
+//mongodb://pronoadmin:pronopassynov@pronofoot-shard-00-00-raenv.mongodb.net:27017,pronofoot-shard-00-01-raenv.mongodb.net:27017,pronofoot-shard-00-02-raenv.mongodb.net:27017/admin?replicaSet=pronofoot-shard-0&ssl=true
 
 //   database: 'mongodb://lexx:lexx@ds127375.mlab.com:27375/reservation',
 
