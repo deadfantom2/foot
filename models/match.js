4b36e158b55acf5976071b0111b62e675ba39e14
bachirbiaich <bachir.biaich@gmail.com>
2018-02-22 11:28:45 +0100

    ajout date match

--- models/match.js
+++ models/match.js
@@ -7,9 +7,9 @@ var MatchSchema = mongoose.Schema({
     equipe1_id:     { type: Schema.Types.ObjectId, ref: 'equipes', required: true },
     equipe2_id:     { type: Schema.Types.ObjectId, ref: 'equipes', required: true },
     goals1:         { type: String, required: true },
-    goals2:         { type: String, required: true }
+    goals2:         { type: String, required: true },
+    date:           { type: Date, required: true },
 });
-
 
 
 var Match = mongoose.model('matchs', MatchSchema);
