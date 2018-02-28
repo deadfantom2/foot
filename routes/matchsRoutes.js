b51ed3f1cbb8a4f47cf6f0fdc92960e3953e314f
deadfantom2 <cameratest81@gmail.com>
2018-02-28 10:37:57 +0100

    add populate sur matchs/:id pour voir les noms des équipes

--- routes/matchsRoutes.js
+++ routes/matchsRoutes.js
@@ -31,20 +31,20 @@ router.get('/', function(req, res, next) {
 /* GET match by id */
 router.get('/:_id', function(req, res, next) {
     var response = { hasErrors: false, data: {}, message: ""};
-    Match.findOne({_id: req.params._id},'',function(err,match){
-      if (err)
-      {
-        response.hasErrors = true;
-        response.message = err; 
-        res.status(404).send(response);
-      }
-      else
-      {
-        response.data = match;
-        res.send(response);
-      }
+    Match.findOne({_id: req.params._id}).populate('equipe1_id').populate('equipe2_id').exec(function(err,match){
+        if (err)
+        {
+            response.hasErrors = true;
+            response.message = err;
+            res.status(404).send(response);
+        }
+        else
+        {
+            response.data = match;
+            res.send(response);
+        }
     });
-  });
+});
   
 /* POST new match*/
 router.post('/', function(req, res, next) {
