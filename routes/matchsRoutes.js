6ae85dfcf26c704a1dbacef0a44d1c32535a6a2e
deadfantom2 <cameratest81@gmail.com>
2018-03-01 12:02:26 +0100

    isAdmin sur les routes /matchs

--- routes/matchsRoutes.js
+++ routes/matchsRoutes.js
@@ -15,14 +15,14 @@ router.get('/', function(req, res, next) {
     Match.find(req.query).populate('equipe1_id').populate('equipe2_id').exec(function(err,matchs){
         if (err)
         {
-          response.hasErrors = true;
-          response.message = err; 
-          res.status(404).send(response);
+            response.hasErrors = true;
+            response.message = err;
+            res.status(404).send(response);
         }
         else
         {
-          response.data = matchs;
-          res.send(response);
+            response.data = matchs;
+            res.send(response);
         }
     });
 });
@@ -45,104 +45,104 @@ router.get('/:_id', function(req, res, next) {
         }
     });
 });
-  
+
 /* POST new match*/
 router.post('/', function(req, res, next) {
     var response = { hasErrors: false, data: {}, message: ""};
     var newMatch = new Match(req.body);
     newMatch.save(function(err,data){
-      if (err)
-      {
-        response.hasErrors = true;
-        response.message = err; 
-        res.status(400).send(response);
-      }
-      else
-      {
-        response.data = data;
-        res.location(`/matchs/${data._id}`);
-        res.status(201).send(response);
-      }
+        if (err)
+        {
+            response.hasErrors = true;
+            response.message = err;
+            res.status(400).send(response);
+        }
+        else
+        {
+            response.data = data;
+            res.location(`/matchs/${data._id}`);
+            res.status(201).send(response);
+        }
     });
-  });
-  
+});
+
 /* PATCH match by id */
 router.patch('/:_id', function(req, res, next) {
-  var response = { hasErrors: false, data: {}, message: ""};
-  Match.update({_id: req.params._id},req.body,{multi: false},function(err,data){
-    if (err)
-    {
-      response.hasErrors = true;
-      response.message = err; 
-      res.status(404).send(response);
-    }
-    else
-    {
-      //1) MAJ des points pour chaque prono du match
-      if(data.goals1 != "" && data.goals2 != "")
-      {
-        Prono.find({match_id: req.params._id}).populate('match_id').populate('utilisateur_id').exec(function(err,pronos){
-          if (err)
-          {
+    var response = { hasErrors: false, data: {}, message: ""};
+    Match.update({_id: req.params._id},req.body,{multi: false},function(err,data){
+        if (err)
+        {
             response.hasErrors = true;
-            response.message = err; 
-            res.status(400).send(response);
-          }
-          else
-          {
-            pronos.forEach(prono => {
-              let points = 0;
-              //RULES
-              if(prono.goals1 == prono.match_id.goals1 && prono.goals2 == prono.match_id.goals2) points = 6; //score parfait
-              else if((prono.goals1 > prono.goals2 && prono.match_id.goals1 > prono.match_id.goals2) || (prono.goals2 > prono.goals1 && prono.match_id.goals2 > prono.match_id.goals1)) points = 3; //vainqueur trouvé
-              else if(prono.goals1 == prono.goals2 && prono.match_id.goals1 == prono.match_id.goals2) points = 3; //nul mais pas le bon score
-              
-              prono.points = points;
-              prono.save(function(err) {
-                if (err) 
-                {
-                  response.hasErrors = true;
-                  response.message = err; 
-                  res.status(400).send(response);
-                }
-              });
-            });
-            //2) MAJ des points pour tous les utilisateurs
-            User.find().exec(function(err,users){
-              if (err)
-              {
-                response.hasErrors = true;
-                response.message = err; 
-                res.status(400).send(response);
-              }
-              else
-              {
-                users.forEach(user => {
-                  user.syncPoints();
+            response.message = err;
+            res.status(404).send(response);
+        }
+        else
+        {
+            //1) MAJ des points pour chaque prono du match
+            if(data.goals1 != "" && data.goals2 != "")
+            {
+                Prono.find({match_id: req.params._id}).populate('match_id').populate('utilisateur_id').exec(function(err,pronos){
+                    if (err)
+                    {
+                        response.hasErrors = true;
+                        response.message = err;
+                        res.status(400).send(response);
+                    }
+                    else
+                    {
+                        pronos.forEach(prono => {
+                            let points = 0;
+                        //RULES
+                        if(prono.goals1 == prono.match_id.goals1 && prono.goals2 == prono.match_id.goals2) points = 6; //score parfait
+                        else if((prono.goals1 > prono.goals2 && prono.match_id.goals1 > prono.match_id.goals2) || (prono.goals2 > prono.goals1 && prono.match_id.goals2 > prono.match_id.goals1)) points = 3; //vainqueur trouvé
+                        else if(prono.goals1 == prono.goals2 && prono.match_id.goals1 == prono.match_id.goals2) points = 3; //nul mais pas le bon score
+
+                        prono.points = points;
+                        prono.save(function(err) {
+                            if (err)
+                            {
+                                response.hasErrors = true;
+                                response.message = err;
+                                res.status(400).send(response);
+                            }
+                        });
+                    });
+                        //2) MAJ des points pour tous les utilisateurs
+                        User.find().exec(function(err,users){
+                            if (err)
+                            {
+                                response.hasErrors = true;
+                                response.message = err;
+                                res.status(400).send(response);
+                            }
+                            else
+                            {
+                                users.forEach(user => {
+                                    user.syncPoints();
+                            });
+                            }
+                        });
+                    }
                 });
-              }
-            });
-          }
-        });
-      }
-      response.data = data;
-      res.send(response);
-    }
-  });
+            }
+            response.data = data;
+            res.send(response);
+        }
+    });
 });
-  
+
 /* DELETE match by id */
 router.delete('/:_id', function(req, res, next) {
     var response = { hasErrors: false, data: {}, message: ""};
     Match.remove({_id: req.params._id},function(err){
-      if (err)
-      {
-        response.hasErrors = true;
-        response.message = err; 
-        res.status(404).send(response);
-      }
-      else
-        res.status(204).send(response);
+        if (err)
+        {
+            response.hasErrors = true;
+            response.message = err;
+            res.status(404).send(response);
+        }
+        else
+            res.status(204).send(response);
     });
 });
 
